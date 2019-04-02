ea43b84eabc175435729acebc8d03532aa6c7264
JanHuangLong <janhuanglong@gmail.com>
2019-04-02 12:52:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/main_page/Post.java
@@ -0,0 +1,19 @@
+package com.example.main_page;
+
+/**
+        * Created by evanhou on 2016/5/26.
+        */
+public class Post {
+
+    public String posterName;
+    public String posterThumbnailUrl;
+    public String content;
+
+    public Post(String posterName,
+                String posterThumbnailUrl, String content) {
+        this.posterName = posterName;
+        this.posterThumbnailUrl = posterThumbnailUrl;
+        this.content = content;
+    }
+
+}
